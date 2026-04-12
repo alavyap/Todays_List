545e197f4763aa3ff87301dcb36594b6e326b287
Alavya Pandey <alavya.pandey@gmail.com>
2026-04-12 12:18:59 +0530

    started logical part

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-console.log("Hello, World! This is a simple JavaScript file.");
+
+// Ensures the DOM is fully loaded before running the script
+document.addEventListener("DOMContentLoaded", () => {
+    const taskInput = document.getElementById("task-input");
+    const addTaskBtn = document.getElementById("add-task-btn");
+    const taskList = document.getElementById("task-list");
+    const emptyImage = document.querySelector(".empty-image");
+
+    // Toggle the visibility of the empty image based on the number of tasks
+    const toggleEmptyImage = () => {
+        emptyImage.style.display = taskList.children.length === 0 ? "block" : "none";
+    }
+
+    // Function to add a new task to the list
+    const addTask = (event) => {
+
+        event.preventDefault(); // Prevent form submission if the button is inside a form
+
+        const tastkText = taskInput.value.trim();
+        
+        if (!tastkText) return; // Prevent adding empty tasks
+        
+        const li = document.createElement("li");
+        li.innerHTML = `
+        <input type="checkbox" class="checkbox">
+        <span>${tastkText}</span>
+        `;
+
+        taskList.appendChild(li);
+
+        taskInput.value = ""; // Clear the input field after adding a task
+
+        toggleEmptyImage(); // Update the visibility of the empty image
+        
+    };
+
+    // Event listener for the "Add Task" button
+    addTaskBtn.addEventListener("click", addTask);
+    taskInput.addEventListener("keypress", (e) => {
+        if (e.key == "Enter"){
+            addTask(e);
+        }
+    });
+});
